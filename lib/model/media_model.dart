4d166bc4fab5659d12741b095297bb19abdfa918
matheusqueirozdev <dev.matheusqueiroz@gmail.com>
2024-05-03 18:47:26 -0300

    feat: created MediaModel

---
+++ lib/model/media_model.dart
@@ -0,0 +1,130 @@
+class MediaModel {
+  String title;
+  String description;
+  String urlSmallBanner;
+  String urlLongBanner;
+  bool featured; //FILMES e Series 'Destaque'
+  bool trending; //FILMES e Series 'Em alta'
+
+  MediaModel({
+    required this.title,
+    required this.description,
+    required this.urlSmallBanner,
+    required this.urlLongBanner,
+    required this.featured,
+    required this.trending,
+  });
+}
+
+List<MediaModel> listMedia = [
+  MediaModel(
+    title: 'Vingadores',
+    description:
+        'Loki, o irmão de Thor, ganha acesso ao poder ilimitado do cubo cósmico ao roubá-lo de dentro das instalações da S.H.I.E.L.D. Nick Fury, o diretor desta agência internacional que mantém a paz, logo reúne os únicos super-heróis que serão capazes de defender a Terra de ameaças sem precedentes. Homem de Ferro, Capitão América, Hulk, Thor, Viúva Negra e Gavião Arqueiro formam o time dos sonhos de Fury, mas eles precisam aprender a colocar os egos de lado e agir como um grupo em prol da humanidade.',
+    urlSmallBanner:
+        'https://br.web.img2.acsta.net/c_310_420/pictures/19/04/26/17/30/2428965.jpg',
+    urlLongBanner:
+        'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/66D8BB10CAF60D83CF4C046E8275561A7FD66BC220A06DF413AF54C942B323DD/scale?width=800&aspectRatio=1.78&format=png',
+    featured: true,
+    trending: false,
+  ),
+  MediaModel(
+    title: 'Vingadores',
+    description:
+        'Loki, o irmão de Thor, ganha acesso ao poder ilimitado do cubo cósmico ao roubá-lo de dentro das instalações da S.H.I.E.L.D. Nick Fury, o diretor desta agência internacional que mantém a paz, logo reúne os únicos super-heróis que serão capazes de defender a Terra de ameaças sem precedentes. Homem de Ferro, Capitão América, Hulk, Thor, Viúva Negra e Gavião Arqueiro formam o time dos sonhos de Fury, mas eles precisam aprender a colocar os egos de lado e agir como um grupo em prol da humanidade.',
+    urlSmallBanner:
+        'https://br.web.img2.acsta.net/c_310_420/pictures/19/04/26/17/30/2428965.jpg',
+    urlLongBanner:
+        'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/66D8BB10CAF60D83CF4C046E8275561A7FD66BC220A06DF413AF54C942B323DD/scale?width=800&aspectRatio=1.78&format=png',
+    featured: true,
+    trending: false,
+  ),
+  MediaModel(
+    title: 'Vingadores',
+    description:
+        'Loki, o irmão de Thor, ganha acesso ao poder ilimitado do cubo cósmico ao roubá-lo de dentro das instalações da S.H.I.E.L.D. Nick Fury, o diretor desta agência internacional que mantém a paz, logo reúne os únicos super-heróis que serão capazes de defender a Terra de ameaças sem precedentes. Homem de Ferro, Capitão América, Hulk, Thor, Viúva Negra e Gavião Arqueiro formam o time dos sonhos de Fury, mas eles precisam aprender a colocar os egos de lado e agir como um grupo em prol da humanidade.',
+    urlSmallBanner:
+        'https://br.web.img2.acsta.net/c_310_420/pictures/19/04/26/17/30/2428965.jpg',
+    urlLongBanner:
+        'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/66D8BB10CAF60D83CF4C046E8275561A7FD66BC220A06DF413AF54C942B323DD/scale?width=800&aspectRatio=1.78&format=png',
+    featured: true,
+    trending: false,
+  ),
+  MediaModel(
+    title: 'Vingadores',
+    description:
+        'Loki, o irmão de Thor, ganha acesso ao poder ilimitado do cubo cósmico ao roubá-lo de dentro das instalações da S.H.I.E.L.D. Nick Fury, o diretor desta agência internacional que mantém a paz, logo reúne os únicos super-heróis que serão capazes de defender a Terra de ameaças sem precedentes. Homem de Ferro, Capitão América, Hulk, Thor, Viúva Negra e Gavião Arqueiro formam o time dos sonhos de Fury, mas eles precisam aprender a colocar os egos de lado e agir como um grupo em prol da humanidade.',
+    urlSmallBanner:
+        'https://br.web.img2.acsta.net/c_310_420/pictures/19/04/26/17/30/2428965.jpg',
+    urlLongBanner:
+        'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/66D8BB10CAF60D83CF4C046E8275561A7FD66BC220A06DF413AF54C942B323DD/scale?width=800&aspectRatio=1.78&format=png',
+    featured: false,
+    trending: true,
+  ),
+  MediaModel(
+    title: 'Vingadores',
+    description:
+        'Loki, o irmão de Thor, ganha acesso ao poder ilimitado do cubo cósmico ao roubá-lo de dentro das instalações da S.H.I.E.L.D. Nick Fury, o diretor desta agência internacional que mantém a paz, logo reúne os únicos super-heróis que serão capazes de defender a Terra de ameaças sem precedentes. Homem de Ferro, Capitão América, Hulk, Thor, Viúva Negra e Gavião Arqueiro formam o time dos sonhos de Fury, mas eles precisam aprender a colocar os egos de lado e agir como um grupo em prol da humanidade.',
+    urlSmallBanner:
+        'https://br.web.img2.acsta.net/c_310_420/pictures/19/04/26/17/30/2428965.jpg',
+    urlLongBanner:
+        'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/66D8BB10CAF60D83CF4C046E8275561A7FD66BC220A06DF413AF54C942B323DD/scale?width=800&aspectRatio=1.78&format=png',
+    featured: false,
+    trending: true,
+  ),
+  MediaModel(
+    title: 'Vingadores',
+    description:
+        'Loki, o irmão de Thor, ganha acesso ao poder ilimitado do cubo cósmico ao roubá-lo de dentro das instalações da S.H.I.E.L.D. Nick Fury, o diretor desta agência internacional que mantém a paz, logo reúne os únicos super-heróis que serão capazes de defender a Terra de ameaças sem precedentes. Homem de Ferro, Capitão América, Hulk, Thor, Viúva Negra e Gavião Arqueiro formam o time dos sonhos de Fury, mas eles precisam aprender a colocar os egos de lado e agir como um grupo em prol da humanidade.',
+    urlSmallBanner:
+        'https://br.web.img2.acsta.net/c_310_420/pictures/19/04/26/17/30/2428965.jpg',
+    urlLongBanner:
+        'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/66D8BB10CAF60D83CF4C046E8275561A7FD66BC220A06DF413AF54C942B323DD/scale?width=800&aspectRatio=1.78&format=png',
+    featured: false,
+    trending: true,
+  ),
+  MediaModel(
+    title: 'Vingadores',
+    description:
+        'Loki, o irmão de Thor, ganha acesso ao poder ilimitado do cubo cósmico ao roubá-lo de dentro das instalações da S.H.I.E.L.D. Nick Fury, o diretor desta agência internacional que mantém a paz, logo reúne os únicos super-heróis que serão capazes de defender a Terra de ameaças sem precedentes. Homem de Ferro, Capitão América, Hulk, Thor, Viúva Negra e Gavião Arqueiro formam o time dos sonhos de Fury, mas eles precisam aprender a colocar os egos de lado e agir como um grupo em prol da humanidade.',
+    urlSmallBanner:
+        'https://br.web.img2.acsta.net/c_310_420/pictures/19/04/26/17/30/2428965.jpg',
+    urlLongBanner:
+        'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/66D8BB10CAF60D83CF4C046E8275561A7FD66BC220A06DF413AF54C942B323DD/scale?width=800&aspectRatio=1.78&format=png',
+    featured: false,
+    trending: true,
+  ),
+  MediaModel(
+    title: 'Vingadores',
+    description:
+        'Loki, o irmão de Thor, ganha acesso ao poder ilimitado do cubo cósmico ao roubá-lo de dentro das instalações da S.H.I.E.L.D. Nick Fury, o diretor desta agência internacional que mantém a paz, logo reúne os únicos super-heróis que serão capazes de defender a Terra de ameaças sem precedentes. Homem de Ferro, Capitão América, Hulk, Thor, Viúva Negra e Gavião Arqueiro formam o time dos sonhos de Fury, mas eles precisam aprender a colocar os egos de lado e agir como um grupo em prol da humanidade.',
+    urlSmallBanner:
+        'https://br.web.img2.acsta.net/c_310_420/pictures/19/04/26/17/30/2428965.jpg',
+    urlLongBanner:
+        'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/66D8BB10CAF60D83CF4C046E8275561A7FD66BC220A06DF413AF54C942B323DD/scale?width=800&aspectRatio=1.78&format=png',
+    featured: false,
+    trending: true,
+  ),
+  MediaModel(
+    title: 'Vingadores',
+    description:
+        'Loki, o irmão de Thor, ganha acesso ao poder ilimitado do cubo cósmico ao roubá-lo de dentro das instalações da S.H.I.E.L.D. Nick Fury, o diretor desta agência internacional que mantém a paz, logo reúne os únicos super-heróis que serão capazes de defender a Terra de ameaças sem precedentes. Homem de Ferro, Capitão América, Hulk, Thor, Viúva Negra e Gavião Arqueiro formam o time dos sonhos de Fury, mas eles precisam aprender a colocar os egos de lado e agir como um grupo em prol da humanidade.',
+    urlSmallBanner:
+        'https://br.web.img2.acsta.net/c_310_420/pictures/19/04/26/17/30/2428965.jpg',
+    urlLongBanner:
+        'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/66D8BB10CAF60D83CF4C046E8275561A7FD66BC220A06DF413AF54C942B323DD/scale?width=800&aspectRatio=1.78&format=png',
+    featured: false,
+    trending: true,
+  ),
+  MediaModel(
+    title: 'Vingadores',
+    description:
+        'Loki, o irmão de Thor, ganha acesso ao poder ilimitado do cubo cósmico ao roubá-lo de dentro das instalações da S.H.I.E.L.D. Nick Fury, o diretor desta agência internacional que mantém a paz, logo reúne os únicos super-heróis que serão capazes de defender a Terra de ameaças sem precedentes. Homem de Ferro, Capitão América, Hulk, Thor, Viúva Negra e Gavião Arqueiro formam o time dos sonhos de Fury, mas eles precisam aprender a colocar os egos de lado e agir como um grupo em prol da humanidade.',
+    urlSmallBanner:
+        'https://br.web.img2.acsta.net/c_310_420/pictures/19/04/26/17/30/2428965.jpg',
+    urlLongBanner:
+        'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/66D8BB10CAF60D83CF4C046E8275561A7FD66BC220A06DF413AF54C942B323DD/scale?width=800&aspectRatio=1.78&format=png',
+    featured: false,
+    trending: true,
+  ),
+];
