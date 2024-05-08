677cba6a9ae29a915150468b953425f187a7a181
Wilmar Lima <elasawilmar@gmail.com>
2024-05-08 16:18:11 -0300

    feat: Criei a pagina de pesquisa e o input da pesquisa

---
+++ lib/components/pesquisar_input.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class PesquisarInput extends StatelessWidget {
+  // ignore: overridden_fields
+  final TextEditingController? controller;
+
+  const PesquisarInput({
+    super.key,
+    this.controller,
+    this.style,
+  });
+
+  final TextStyle? style;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: const Color(0xFF1F1F1F),
+        borderRadius: BorderRadius.circular(15),
+        border: Border.all(color: Colors.transparent),
+      ),
+      child: TextField(
+        controller: controller,
+        style: style,
+        cursorColor: Colors.white,
+        decoration: const InputDecoration(
+          border: InputBorder.none,
+          hintText: '',
+          contentPadding: EdgeInsets.symmetric(horizontal: 25),
+        ),
+      ),
+    );
+  }
+}
