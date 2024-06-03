bcbaa2b1c29ace2df66f25319eece33fda6f93f1
Matheus Queiroz <dev.matheusqueiroz@gmail.com>
2024-06-03 19:44:26 -0300

    fix: correcao na busca

--- lib/view/search_page.dart
+++ lib/view/search_page.dart
@@ -4,6 +4,7 @@ import 'package:dart_movies_app/repositories/discover_movie_repository.dart';
 import 'package:dart_movies_app/components/appbar_custom.dart';
 import 'package:dart_movies_app/components/research_card.dart';
 import 'package:dart_movies_app/components/small_card.dart';
+import 'package:dart_movies_app/view/detail_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -22,6 +23,8 @@ class SearchPageState extends State<SearchPage> {
   late MediaBloc _mediaBloc;
   int paginaCorrente = 1;
   final ScrollController _scrollController = ScrollController();
+
+  List<MovieModel> filmes = [];
 
   @override
   void initState() {
@@ -57,69 +60,85 @@ class SearchPageState extends State<SearchPage> {
                 ResearchCard(
                   controller: widget.controller,
                   onChanged: (String value) {
-                    setState(() {
-                      _mediaBloc.add(SearchMovieEvent(value));
-                    });
+                    if (value == '') {
+                      paginaCorrente = 1;
+                      _mediaBloc.add(LoadMoreMovies(paginaCorrente));
+                    }
+
+                    _mediaBloc.add(SearchMovieEvent(value));
                   },
                 ),
                 Expanded(
                   child: Padding(
                     padding: const EdgeInsets.symmetric(
                         horizontal: 10, vertical: 25),
-                    child: BlocBuilder<MediaBloc, MediaState>(
+                    child: BlocConsumer<MediaBloc, MediaState>(
                       bloc: _mediaBloc,
+                      listener: (context, state) {
+                        if (state is SearchSuccessState) {
+                          filmes = state.discoverMovieModel;
+                        }
+                      },
                       builder: (context, state) {
                         if (state is SearchLoadingState &&
                             paginaCorrente == 1) {
                           return const Center(
                               child: CircularProgressIndicator(
                                   color: Colors.white));
-                        } else if (state is SearchSuccessState) {
-                          return NotificationListener<ScrollNotification>(
-                            onNotification: (ScrollNotification scrollInfo) {
-                              if (scrollInfo.metrics.pixels ==
-                                  scrollInfo.metrics.maxScrollExtent) {
-                                if (state is! LoadMoreLoadingState) {
-                                  paginaCorrente++;
-                                  _mediaBloc
-                                      .add(LoadMoreMovies(paginaCorrente));
-                                }
-                                return true;
-                              }
-                              return false;
-                            },
-                            child: GridView.builder(
-                              controller: _scrollController,
-                              gridDelegate:
-                                  const SliverGridDelegateWithFixedCrossAxisCount(
-                                crossAxisCount: 3,
-                                childAspectRatio: 35 / 50,
-                              ),
-                              itemCount: state.discoverMovieModel.length,
-                              itemBuilder: (context, index) {
-                                MovieModel trendingMedia =
-                                    state.discoverMovieModel[index];
-
-                                return GestureDetector(
-                                  onTap: () {},
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(5),
-                                    child: SmallCard(
-                                      imageUrl:
-                                          'https://image.tmdb.org/t/p/w200/${trendingMedia.posterPath}',
-                                    ),
-                                  ),
-                                );
-                              },
-                              shrinkWrap: true,
-                              physics: const AlwaysScrollableScrollPhysics(),
-                            ),
-                          );
-                        } else if (state is SearchErrorState) {
-                          return Center(child: Text('Erro: ${state.message}'));
-                        } else {
-                          return Container();
                         }
+
+                        if (state is SearchSuccessState) {}
+                        if (state is SearchErrorState) {
+                          return Center(child: Text('Erro: ${state.message}'));
+                        }
+                        return NotificationListener<ScrollNotification>(
+                          onNotification: (ScrollNotification scrollInfo) {
+                            if (scrollInfo.metrics.pixels ==
+                                scrollInfo.metrics.maxScrollExtent) {
+                              if (state is! LoadMoreLoadingState) {
+                                paginaCorrente++;
+                                _mediaBloc.add(LoadMoreMovies(paginaCorrente));
+                              }
+                              return true;
+                            }
+                            return false;
+                          },
+                          child: GridView.builder(
+                            controller: _scrollController,
+                            gridDelegate:
+                                const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 3,
+                              childAspectRatio: 35 / 50,
+                            ),
+                            itemCount: filmes.length,
+                            itemBuilder: (context, index) {
+                              MovieModel trendingMedia = filmes[index];
+
+                              return GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => DetailPage(
+                                        movieModel: trendingMedia,
+                                        recommendeds: filmes,
+                                      ),
+                                    ),
+                                  );
+                                },
+                                child: Padding(
+                                  padding: const EdgeInsets.all(5),
+                                  child: SmallCard(
+                                    imageUrl:
+                                        'https://image.tmdb.org/t/p/w200/${trendingMedia.posterPath}',
+                                  ),
+                                ),
+                              );
+                            },
+                            shrinkWrap: true,
+                            physics: const AlwaysScrollableScrollPhysics(),
+                          ),
+                        );
                       },
                     ),
 
